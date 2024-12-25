b23e5b91e03ee7818a0af394183117e82e5e77d6
alex_barmen <www.lad282@gmail.com>
2024-12-25 19:12:06 +0300

    feat(api): Subject model registered on admin panel

--- edu/courses/admin.py
+++ edu/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'updated_at', 'created_at']
+    list_display_links = ['title']
+    search_fields = ['title', 'slug']
