0c0f56de6077fe57b5ac3b84cb9bfac2fe70e0d9
alex_barmen <www.lad282@gmail.com>
2024-12-25 19:07:56 +0300

    feat(api): subject model and migration

--- edu/courses/models.py
+++ edu/courses/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from utils.mixins.auto_date import AutoDateMixin
+
+
+class Subject(AutoDateMixin):
+    """Модель предмета, по которому идут курсы"""
+    
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Слаг',
+    )
+    
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+        
+    def __str__(self):
+        return self.title
