7766db021656df1a327df1d29c91773cea74b074
alex_barmen <www.lad282@gmail.com>
2024-12-25 19:22:25 +0300

    feat(api): Course model registered at the admin

--- edu/courses/admin.py
+++ edu/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from courses.models import Subject
+from courses.models import Course, Subject
 
 
 @admin.register(Subject)
@@ -8,3 +8,12 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'slug', 'updated_at', 'created_at']
     list_display_links = ['title']
     search_fields = ['title', 'slug']
+    
+    
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """Админ для Course"""
+    list_display = ['id', 'title', 'owner', 'updated_at', 'created_at']
+    list_display_links = ['title']
+    raw_id_fields = ['owner', 'subject']
+    search_fields = ['title', 'owner', 'overview']
