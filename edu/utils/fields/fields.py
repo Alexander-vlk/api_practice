addbc42ecb88330f865fcb918b4666c277a7f180
alex_barmen <www.lad282@gmail.com>
2024-12-26 20:12:42 +0300

    feat(api): OrderField applied field

---
+++ edu/utils/fields/fields.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class OrderField(models.PositiveBigIntegerField):
+    """Поле порядка следования элементов курса"""
+    
+    def __init__(self, for_fields=None, *args, **kwargs):
+        """Расширение метода __init__"""
+        self.for_fields = for_fields
+        super().__init__(*args, **kwargs)
+        
+    def pre_save(self, model_instance, add):
+        """Расширение метода pre_save"""
+        if not getattr(model_instance, self.attname):
+            return super().pre_save(model_instance, add)
+        
+        queryset = self.model.objects.all()
+        try:
+            if self.for_fields:
+                query_dict = {field: getattr(model_instance, field) for field in self.for_fields}
+                queryset = queryset.filter(**query_dict)
+                
+            last_item = queryset.latest(self.attname)
+            value = last_item.order + 1
+        except ObjectDoesNotExist:
+            value = 0
+            
+        setattr(model_instance, self.attname, value)
+        
+        return value
+
+        
