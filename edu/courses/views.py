3f04a78935102f57e14949bfaddde20e5a029889
alex_barmen <www.lad282@gmail.com>
2024-12-26 23:54:16 +0300

    feat(api): CRUD views for course

--- edu/courses/views.py
+++ edu/courses/views.py
@@ -1,6 +1,13 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import (
+    CreateView,
+    DeleteView,
+    UpdateView,
+)
 from django.views.generic.list import ListView
 
 from courses.models import Course
+from courses.mixins import OwnerCourseMixin, OwnerCourseEditMixin
 
 
 class ManageCourseListView(ListView):
@@ -12,3 +19,15 @@ class ManageCourseListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(owner=self.request.user)
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
