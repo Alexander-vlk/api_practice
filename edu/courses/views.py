15b543a5fed6f390cdb45567eeb9b0e74bb0bf93
alex_barmen <www.lad282@gmail.com>
2024-12-27 21:30:16 +0300

    feat(api): ModuleContentListView

--- edu/courses/views.py
+++ edu/courses/views.py
@@ -172,3 +172,17 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module.id)
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    """View для списка модулей"""
+    
+    template_name = 'courses/manage/module/content_list.html'
+    
+    def get(self, request, module_id):
+        module = get_object_or_404(
+            Module, 
+            id=module_id,
+            course__owner=request.user,
+        )
+        return self.render_to_response({'module': module})
