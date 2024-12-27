27d2b79075b5894cd7589e743b61340364ab1389
alex_barmen <www.lad282@gmail.com>
2024-12-27 21:14:00 +0300

    feat(api): update and create module urls

--- edu/courses/urls.py
+++ edu/courses/urls.py
@@ -9,4 +9,16 @@ urlpatterns = [
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
     path('<pk>/module/', views.CourseModuleTemplateView.as_view(), name='course_module_update'),
+    
+    path(
+        'module/<int:module_id>/content/<model_name>/create/',
+        views.ContentCreateUpdateView.as_view(),
+        name='module_content_create',
+         ),
+    path(
+        'module/<int:module_id>/content/<model_name>/<id>/',
+        views.ContentCreateUpdateView.as_view(),
+        name='module_content_update',
+    )
+
 ]
