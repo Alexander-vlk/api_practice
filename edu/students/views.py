46e6ddc2addcb8e6a065a32381a6ebb5d2ca370d
alex_barmen <www.lad282@gmail.com>
2024-12-30 19:49:06 +0300

    feat(api): StudentRegisterView

--- edu/students/views.py
+++ edu/students/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+class StudentRegisterView(CreateView):
+    """View регистрации студентов"""
+    
+    template_name = 'stundents/student/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('student_course_list')
+    
+    def form_valid(self, form):
+        data = form.cleaned_data
+        
+        user = authenticate(
+            username=data['username'],
+            password=data['password1'],
+        )
+        
+        login(self.request, user)
+        
+        return super().form_valid(form)
+    
+    
