094e68ca7919ddb1441de87e0e5dfca97169405f
alex_barmen <www.lad282@gmail.com>
2024-12-31 12:33:26 +0300

    feat(api): urls for student courses

--- edu/students/urls.py
+++ edu/students/urls.py
@@ -5,4 +5,12 @@ from students import views
 urlpatterns = [
     path('register/', views.StudentRegisterView.as_view(), name='register'),
     path('enroll/course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('courses', views.StudentCourseListView.as_view(), name='student_course_list'),
+    path('course/<pk>/', views.StudentCourseDetailView.as_view(), name='student_course_detail'),
+    
+    path(
+        'course/<pk>/<module_id>/', 
+        views.StudentCourseDetailView.as_view(), 
+        name='student_course_detail_module'
+    ),
 ]
