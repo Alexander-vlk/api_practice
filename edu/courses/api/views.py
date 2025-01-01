e57c8e98b3fc09cf0d1f9969013f2df5eade2214
alex_barmen <www.lad282@gmail.com>
2025-01-01 23:59:34 +0300

    feat(api): action to get courses with content if user is enrolled in

--- edu/courses/api/views.py
+++ edu/courses/api/views.py
@@ -7,7 +7,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from courses.models import Course, Subject
-from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import (
+    CourseSerializer,
+    CourseWithContentSerializer,
+    SubjectSerializer,
+)
 
 
 class SubjectListView(generics.ListAPIView):
@@ -40,6 +45,16 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True,})
+    
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class CourseEnrollView(APIView):
