bb4a8b3b69574ccaded6e7338d83f38deab19d3c
alex_barmen <www.lad282@gmail.com>
2025-01-01 21:50:38 +0300

    feat(api): CourseSerializer, ModuleSerializer

--- edu/courses/api/serializers.py
+++ edu/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from courses.models import Subject
+from courses.models import Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -9,3 +9,31 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+        
+
+class ModuleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Module"""
+    
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Course"""
+   
+    modules = ModuleSerializer(many=True, read_only=True)
+   
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created_at',
+            'updated_at',
+            'owner',
+            'modules',
+        ]
