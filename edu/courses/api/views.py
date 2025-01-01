639f0e1159e3645f539dcb0292f0c115f6aa7d33
alex_barmen <www.lad282@gmail.com>
2025-01-01 22:27:40 +0300

    feat(api): CourseViewSet

--- edu/courses/api/views.py
+++ edu/courses/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from courses.models import Course, Subject
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import CourseSerializer, SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -21,6 +21,13 @@ class SubjectDetailView(generics.RetrieveAPIView):
     
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet для модели Course"""
+    
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
 
 class CourseEnrollView(APIView):
